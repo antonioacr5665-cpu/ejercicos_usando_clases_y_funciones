a6110ce52979779c5ac26fb0885c8bd629faba45
car8092361628 <antonioacr5665>
2025-12-12 17:51:08 -0400

    ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,7 +2,7 @@ class Rectangulo:
     def __init__(self, base, altura):
         self.base = base
         self.altura = altura
-    
+    #
     def calcular_area(self):
         return self.base * self.altura
     
